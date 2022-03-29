44f3bfc64796bbe1c5a27737d08b00f5393404d6
Jeff Self <jeff.self@gmail.com>
2022-03-28 20:47:17 -0400

    Project 1, Part 1

---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Jeff Self on 3/28/22.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
